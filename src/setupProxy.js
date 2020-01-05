86cda3e79737161f565ad8755d314dae8b4bdd62
Manh-Hoang <nguyenhoang56ksgt@gmail.com>
2020-01-05 09:43:21 +0100

    test proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     proxy({
-      target: 'http://localhost:5000',
+      target: 'https://manhhoang-api.herokuapp.com',
       changeOrigin: true,
     }),
   );
--- src/utils/setAuthToken.js
+++ src/utils/setAuthToken.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const setAuthToken = token => {
   if (token) {
     // axios.defaults.headers.common['x-auth-token'] = token;
-    axios.defaults.baseURL = 'https://manhhoang-api.herokuapp.com/';
+    // axios.defaults.baseURL = 'https://manhhoang-api.herokuapp.com/';
     axios.defaults.headers.common.Authorization = `Bearer ${token}`;
   } else {
     delete axios.defaults.headers.common.Authorization;
